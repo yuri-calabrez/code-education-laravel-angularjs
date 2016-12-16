70de496af2537b6f907e6a63d128197a40325fc2
Yuri Calabrez <yuri.calabrez@gmail.com>
2016-12-16 00:06:10 -0200

    Implementando OAuth2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,35 +15,35 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('client', 'ClientController@index');
-Route::post('client', 'ClientController@store');
-Route::get('client/{id}', 'ClientController@show');
-Route::put('client/{id}', 'ClientController@update');
-Route::delete('client/{id}', 'ClientController@destroy');
+Route::post('oauth/access_token', function(){
+   return Response::json(Authorizer::issueAccessToken());
+});
 
-//Project Note
-Route::get('project/{id}/note', 'ProjectNoteController@index');
-Route::post('project/{id}/note', 'ProjectNoteController@store');
-Route::get('project/{id}/note/{noteId}', 'ProjectNoteController@show');
-Route::put('project/{id}/note/{noteId}', 'ProjectNoteController@update');
-Route::delete('project/{id}/note/{noteId}', 'ProjectNoteController@destroy');
+Route::group(['middleware' => 'oauth'], function(){
+    Route::resource('client', 'ClientController', ['except' => ['create', 'edit']]);
 
-//Project Task
-Route::get('project/{id}/task', 'ProjectTaskController@index');
-Route::post('project/{id}/task', 'ProjectTaskController@store');
-Route::get('project/{id}/task/{taskId}', 'ProjectTaskController@show');
-Route::put('project/{id}/task/{taskId}', 'ProjectTaskController@update');
-Route::delete('project/{id}/task/{taskId}', 'ProjectTaskController@destroy');
+    Route::group(['prefix' => 'project'], function(){
+        Route::resource('', 'ProjectController', ['except' => ['create', 'edit']]);
 
-//Members
-Route::get('project/{id}/members', 'ProjectController@members');
-Route::post('project/{id}/member/{memberId}', 'ProjectController@addMember');
-Route::delete('project/{id}/member/{memberId}', 'ProjectController@removeMember');
-Route::get('project/{id}/member/{memberId}', 'ProjectController@isMember');
+        //Project Note
+        Route::get('{id}/note', 'ProjectNoteController@index');
+        Route::post('{id}/note', 'ProjectNoteController@store');
+        Route::get('{id}/note/{noteId}', 'ProjectNoteController@show');
+        Route::put('{id}/note/{noteId}', 'ProjectNoteController@update');
+        Route::delete('{id}/note/{noteId}', 'ProjectNoteController@destroy');
 
-//Project
-Route::get('project', 'ProjectController@index');
-Route::post('project', 'ProjectController@store');
-Route::get('project/{id}', 'ProjectController@show');
-Route::put('project/{id}', 'ProjectController@update');
-Route::delete('project/{id}', 'ProjectController@destroy');
+        //Project Task
+        Route::get('{id}/task', 'ProjectTaskController@index');
+        Route::post('{id}/task', 'ProjectTaskController@store');
+        Route::get('{id}/task/{taskId}', 'ProjectTaskController@show');
+        Route::put('{id}/task/{taskId}', 'ProjectTaskController@update');
+        Route::delete('{id}/task/{taskId}', 'ProjectTaskController@destroy');
+
+        //Members
+        Route::get('{id}/members', 'ProjectController@members');
+        Route::post('{id}/member/{memberId}', 'ProjectController@addMember');
+        Route::delete('{id}/member/{memberId}', 'ProjectController@removeMember');
+        Route::get('{id}/member/{memberId}', 'ProjectController@isMember');
+    });
+
+});
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -11,6 +11,12 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
+        factory(\CodeProject\Entities\User::class)->create([
+            'name' => 'Yuri Calabrez',
+            'email' => 'yuri.calabrez@gmail.com',
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10),
+        ]);
        factory(\CodeProject\Entities\User::class, 10)->create();
     }
 }
